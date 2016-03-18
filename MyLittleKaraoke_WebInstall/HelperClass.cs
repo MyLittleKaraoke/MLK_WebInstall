3e43812cad872befc2e7ad9793d6352a275aa8fa
basisbit <hp.stefan@gmail.com>
2016-03-18 14:12:03 +0100

    replaced the DLL and architecture dependent shortcut-creation-code

--- MyLittleKaraoke_WebInstall/HelperClass.cs
+++ MyLittleKaraoke_WebInstall/HelperClass.cs
@@ -11,7 +11,7 @@ using System.Security.Principal;
 using System.Windows.Forms;
 using Microsoft.VisualBasic;
 using Microsoft.Win32;
-using IWshRuntimeLibrary;
+using System.Runtime.InteropServices.ComTypes;
 using System.Runtime.InteropServices;
 
 namespace MyLittleKaraoke_WebInstall
@@ -171,14 +171,16 @@ namespace MyLittleKaraoke_WebInstall
                 if (!Directory.Exists(appStartMenuPath))
                     Directory.CreateDirectory(appStartMenuPath);
 
-                string shortcutLocation = Path.Combine(appStartMenuPath, ShortCutName + ".lnk");
-                WshShell shell = new WshShell();
-                IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutLocation);
 
-                shortcut.Description = ShortCutName;
-                //shortcut.IconLocation = @"C:\Program Files (x86)\TestApp\TestApp.ico"; //uncomment to set the icon of the shortcut
-                shortcut.TargetPath = ShortCutTarget;
-                shortcut.Save(); 
+                IShellLink link = (IShellLink)new ShellLink();
+
+                // setup shortcut information
+                link.SetDescription(ShortCutName);
+                link.SetPath(ShortCutTarget);
+
+                // save it
+                IPersistFile file = (IPersistFile)link;
+                file.Save(Path.Combine(appStartMenuPath, ShortCutName + ".lnk"), false);
             }
             catch (Exception ex)
             {
@@ -203,6 +205,37 @@ namespace MyLittleKaraoke_WebInstall
             {
                 return false;
             }
+        }
+
+        [ComImport]
+        [Guid("00021401-0000-0000-C000-000000000046")]
+        internal class ShellLink
+        {
+        }
+
+        [ComImport]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        [Guid("000214F9-0000-0000-C000-000000000046")]
+        internal interface IShellLink
+        {
+            void GetPath([Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszFile, int cchMaxPath, out IntPtr pfd, int fFlags);
+            void GetIDList(out IntPtr ppidl);
+            void SetIDList(IntPtr pidl);
+            void GetDescription([Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszName, int cchMaxName);
+            void SetDescription([MarshalAs(UnmanagedType.LPWStr)] string pszName);
+            void GetWorkingDirectory([Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszDir, int cchMaxPath);
+            void SetWorkingDirectory([MarshalAs(UnmanagedType.LPWStr)] string pszDir);
+            void GetArguments([Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszArgs, int cchMaxPath);
+            void SetArguments([MarshalAs(UnmanagedType.LPWStr)] string pszArgs);
+            void GetHotkey(out short pwHotkey);
+            void SetHotkey(short wHotkey);
+            void GetShowCmd(out int piShowCmd);
+            void SetShowCmd(int iShowCmd);
+            void GetIconLocation([Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszIconPath, int cchIconPath, out int piIcon);
+            void SetIconLocation([MarshalAs(UnmanagedType.LPWStr)] string pszIconPath, int iIcon);
+            void SetRelativePath([MarshalAs(UnmanagedType.LPWStr)] string pszPathRel, int dwReserved);
+            void Resolve(IntPtr hwnd, int fFlags);
+            void SetPath([MarshalAs(UnmanagedType.LPWStr)] string pszFile);
         }
     }
 }
