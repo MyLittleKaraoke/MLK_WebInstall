4c6bb977ed8b4684a20e0c0e2dfc6f3316b040d5
basisbit <hp.stefan@gmail.com>
2016-03-23 19:18:29 +0100

    enable the window.MinimizeBox

--- MyLittleKaraoke_WebInstall/Form1.Designer.cs
+++ MyLittleKaraoke_WebInstall/Form1.Designer.cs
@@ -266,7 +266,6 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MaximumSize = new System.Drawing.Size(780, 550);
-            this.MinimizeBox = false;
             this.MinimumSize = new System.Drawing.Size(780, 300);
             this.Name = "Form1";
             this.Text = "My Little Karaoke - Web Installer";
